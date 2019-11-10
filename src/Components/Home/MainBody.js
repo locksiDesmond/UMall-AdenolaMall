71bf60cdf204fb7dae3cc47f8691d4e5f8629698
Locksi <des.olanrewaju@gmail.com>
2019-11-10 17:31:12 +0100

    Added new Pages

---
+++ src/Components/Home/MainBody.js
@@ -0,0 +1,10 @@
+import React from "react";
+import MainImage from './MainImage';
+
+function MainBody() {
+  return <div className="MainBody">
+      <MainImage/>
+      </div>;
+}
+
+export default MainBody;
--- src/Components/Main/MainImage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function MainImage() {
-  return <div className="Main--image"></div>;
-}
-
-export default MainImage;
